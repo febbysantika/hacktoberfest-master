1d329ffc79859a6a06d1cf662df832c4db80b29b
febbysantika <febby.santika@wbi.ac.id>
2024-06-18 17:49:58 +0700

    first commit

---
+++ hacktoberfest-master/hacktober21/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdio.h>
+int main() 
+{
+    //Made chnges to this file.
+    std::cout << "Hello, World! Welcome to the world of programming";
+    return 0;
+}
---
+++ hacktoberfest-master/hacktober21/Join.md
@@ -0,0 +1,1 @@
+Hello and welcome to this repository this is to get creaters attracted to open source.
